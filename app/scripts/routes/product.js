08f45ac0930212e8de464645db12b1a385c56f0b
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-12 11:28:38 -0700

    you can totally add a product to the cart, although it's not smart enough to update the quantity if you've already added that product to your cart before

--- app/scripts/routes/product.js
+++ app/scripts/routes/product.js
@@ -5,17 +5,19 @@ App.ProductRoute = Ember.Route.extend({
 
   actions: {
     addToCart: function(unique_product) {
-      var theCart = this.controllerFor('cart').get('model');
-      var newItem = this.store.createRecord('item', { cart: theCart, product: unique_product, quantity: 1 });
-      newItem.save();
-      theCart.get("items").then(function(items) {
-        items.pushObject(newItem);
-      })
-    }
-  },
+      var newItem = this.store.createRecord('item', {
+        product: unique_product,
+        quantity: 1,
+        current_price: unique_product.get("price")
+      });
 
-  setupController: function(controller, model) {
-    this.controllerFor('cart').set('model', this.store.find('cart', 1));
-    controller.set('model', model);
+      this.store.find("cart", 1).then(function(cart){
+        newItem.set("cart", cart);
+        newItem.save();
+        cart.get("items").then(function(items) {
+          items.pushObject(newItem);
+        });
+      });
+    }
   }
 });
