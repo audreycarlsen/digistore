ce6684db28cefb1c976f0f112df3d625f5544050
Audrey Carlsen <audreycarlsen@gmail.com>
2014-03-19 12:18:13 -0700

    for ellen

--- app/scripts/routes/cart.js
+++ app/scripts/routes/cart.js
@@ -6,6 +6,7 @@ App.CartRoute = Ember.Route.extend({
   actions: {
     more: function (item) {
       item.incrementProperty('quantity');
+      item.save();
     },
     less: function (item) {
       var quantity = item.get('quantity');
@@ -13,13 +14,16 @@ App.CartRoute = Ember.Route.extend({
       if (quantity > 1) {
         item.decrementProperty('quantity');
       }
+
+      item.save();
     },
     removeFromCart: function(item) {
       this.store.find("cart", localStorage.cartId).then(function(cart){
         cart.get("items").then(function(items) {
           items.removeObject(item);
+          item.deleteRecord();
+          item.save();
         });
-        item.deleteRecord();
       });
     }
   }
